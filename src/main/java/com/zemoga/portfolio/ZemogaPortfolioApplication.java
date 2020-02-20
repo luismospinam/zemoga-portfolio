0e1ccf36d9cf3d7c94dbd4bf585933544bf2ed59
Luis Miguel Ospina Munoz <luismiguel.ospina@globant.com>
2020-02-20 13:42:00 -0500

    zemoga-portfolio - Initial commit: creating a new empty Maven Spring boot project.

---
+++ src/main/java/com/zemoga/portfolio/ZemogaPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.zemoga.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZemogaPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZemogaPortfolioApplication.class, args);
+	}
+
+}
