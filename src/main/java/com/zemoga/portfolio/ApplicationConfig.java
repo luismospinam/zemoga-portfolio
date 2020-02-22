852eacca46e8143cad7004f3dcc84f6252ccc735
Luis Miguel Ospina Munoz <luismiguel.ospina@globant.com>
2020-02-21 23:48:50 -0500

    zemoga-portfolio - Adding swager integration for documentation of the endpoints.

--- src/main/java/com/zemoga/portfolio/ApplicationConfig.java
+++ src/main/java/com/zemoga/portfolio/ApplicationConfig.java
@@ -1,15 +1,31 @@
 package com.zemoga.portfolio;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @Configuration
 public class ApplicationConfig {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Docket swaggerConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .build();
     }
 
 }
--- src/main/java/com/zemoga/portfolio/controller/UserPortfolioController.java
+++ src/main/java/com/zemoga/portfolio/controller/UserPortfolioController.java
@@ -2,6 +2,9 @@ package com.zemoga.portfolio.controller;
 
 import com.zemoga.portfolio.model.Portfolio;
 import com.zemoga.portfolio.service.PortfolioService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,6 +25,11 @@ public class UserPortfolioController {
     }
 
 
+    @ApiOperation(value = "Finds a Portfolio by ID and the tweets of its user.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 404, message = "Portfolio not found"),
+            @ApiResponse(code = 500, message = "Server error")})
     @GetMapping(value = "/user_info/{idPortfolio}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Portfolio> getPortfolioById(@PathVariable("idPortfolio") final String idPortfolio) {
         var portfolio = portfolioService.findPortfolioById(idPortfolio);
@@ -29,6 +37,12 @@ public class UserPortfolioController {
         return ResponseEntity.ok(portfolio);
     }
 
+
+    @ApiOperation(value = "Modify a Portfolio by ID with the provided data.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 404, message = "Portfolio not found"),
+            @ApiResponse(code = 500, message = "Server error")})
     @PostMapping(value = "/modify_user_info/{idPortfolio}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Portfolio> modifyPortfolioById(@RequestBody(required = false) final Portfolio portfolio, @PathVariable("idPortfolio") final String idPortfolio) {
         var portfolioResponse = portfolioService.updatePortfolio(portfolio, idPortfolio);
@@ -37,6 +51,9 @@ public class UserPortfolioController {
     }
 
 
+    @ApiOperation(value = "INVALID ENDPOINT should use /modify_user_info/{idPortfolio} instead.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 301, message = "Moved permanently")})
     @PostMapping(value = "/modify_user_info/")
     public ResponseEntity<?> invalidModifyEndpoint() {
         var responseMap = new LinkedHashMap<String, String>();
